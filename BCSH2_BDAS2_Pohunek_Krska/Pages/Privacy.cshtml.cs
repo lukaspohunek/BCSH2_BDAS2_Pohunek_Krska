aadad5fa59b1269b9fa372f04b87b59be2c0e89f
Lukáš Pohůnek <st67090@upce.cz>
2023-10-31 13:51:27 +0100

    vytvoření projektu

---
+++ BCSH2_BDAS2_Pohunek_Krska/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BCSH2_BDAS2_Pohunek_Krska.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
