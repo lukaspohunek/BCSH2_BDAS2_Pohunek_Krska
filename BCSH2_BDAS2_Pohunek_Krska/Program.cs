ffaed4e98c58b4cbd45f08a23b6b38404f2e69a0
Lukáš Pohůnek <st67090@upce.cz>
2023-11-07 12:18:59 +0100

    napojovaci string na DB

--- BCSH2_BDAS2_Pohunek_Krska/Program.cs
+++ BCSH2_BDAS2_Pohunek_Krska/Program.cs
@@ -1,15 +1,44 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Oracle.ManagedDataAccess.Client;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+// Build the application and create a configuration object
 var app = builder.Build();
+var configuration = app.Configuration;
 
-// Configure the HTTP request pipeline.
+// Configure the Oracle connection
+using (OracleConnection connection = GetOracleConnection(configuration))
+{
+    connection.Open();
+
+    // Do your database operations here, such as querying data.
+    // For example:
+    using (OracleCommand command = new OracleCommand("SELECT 'Hello, Oracle!' FROM DUAL", connection))
+    {
+        using (OracleDataReader reader = command.ExecuteReader())
+        {
+            if (reader.Read())
+            {
+                string result = reader.GetString(0);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
+
+// Continue configuring the HTTP request pipeline, routing, and run the application as needed.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -22,10 +51,13 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 
-// Add your custom route mapping here
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
 app.Run();
+
+// Method to configure the Oracle connection
+OracleConnection GetOracleConnection(IConfiguration configuration)
+{
+    string connectionString = configuration.GetConnectionString("OracleConnection");
+    return new OracleConnection(connectionString);
+}
+
 
